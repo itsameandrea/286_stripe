50a15321f5e3e868647840a20e2a2f49284fa95d
Andrea Rocca <andrea.rocca3@gmail.com>
2019-08-26 10:39:11 +0200

    lecture

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root 'teddies#index'
+  resources :teddies, only: [:index, :show]
+  resources :orders, only: [:show, :create] do
+    resources :payments, only: [:new, :create]
+  end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
